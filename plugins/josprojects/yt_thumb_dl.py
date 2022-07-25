44b90bdcd58702302be30903217a2fa885d84c17
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-07-25 16:00:13 +0530

    Update yt_thumb_dl.py

--- plugins/josprojects/yt_thumb_dl.py
+++ plugins/josprojects/yt_thumb_dl.py
@@ -40,13 +40,13 @@ async def send_thumbnail(bot, update):
         )
         await update.reply_photo(
             photo=thumbnail,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join official channel', url='https://telegram.me/mastermindmayankproject')]]),
             quote=True
         )
         await message.delete()
     except Exception as error:
         await message.edit_text(
-            text="**Please Use** /ytthumb (youtube link)\n\n**Example:** `/ytthumb https://youtu.be/h6PtzFYaMxQ`",
+            text="**Please Use** /ytthumb (youtube link)\n\n**Example:** `/ytthumb https://youtu.be/h6PtYaMxQ`",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join projects channel', url='https://telegram.me/josprojects')]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('join official channel', url='https://telegram.me/mastermindmayankproject')]])
         )
