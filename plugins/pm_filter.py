88a27b8bfb14fbffc86d731f4b3be1f526d576a7
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-08-12 07:12:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -88,7 +88,7 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
-     btn.insert(0, 
+      btn.insert(0, 
         [
             InlineKeyboardButton(f'🎬 {search} 🎬', 'reqst1')
         ]
